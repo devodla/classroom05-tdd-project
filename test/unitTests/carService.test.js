ba25803681a540877c40a31a77e99b54f282ac25
Huillca Reynaldo <lymreynaldo@icloud.com>
2023-02-07 07:02:00 -0300

    test: add transaction and las test

--- test/unitTests/carService.test.js
+++ test/unitTests/carService.test.js
@@ -3,6 +3,7 @@ const { expect } = require("chai");
 const { describe, it, before, beforeEach, afterEach } = require("mocha");
 const sinon = require("sinon");
 const CarService = require("./../../src/service/carService");
+const Transaction = require("./../../src/entities/transaction");
 
 const carsDatabase = join(__dirname, "./../../database", "cars.json");
 const mocks = {
@@ -82,6 +83,39 @@ describe("CarService Suite Tests", () => {
     // console.log("taxes", carService.taxesBasedOnAge);
     const expected = carService.currencyFormat.format(244.4);
     const result = carService.calculateFinalPrice(customer, carCategory, numberOfDays);
+
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("given a customer and a car category it should return a transaction receipt", async () => {
+    const car = mocks.validCar;
+    const carCategory = {
+      ...mocks.validCarCategory,
+      price: 37.6,
+      carIds: [car.id],
+    };
+
+    const customer = Object.create(mocks.validCustomer);
+    customer.age = 20;
+
+    const numberOfDays = 5;
+    const dueDate = "10 de novembro de 2020";
+    const now = new Date(2020, 10, 5);
+    sandox.useFakeTimers(now.getTime());
+
+    // age 20, tax: 1.1, category price: 37.6
+    // 37.6 * 1.1 = 41.36 * 5 days = 206.8
+
+    sandox.stub(carService.carRepository, carService.carRepository.find.name).resolves(car);
+    const expectedAmount = carService.currencyFormat.format(206.8);
+    const result = await carService.rent(customer, carCategory, numberOfDays);
+
+    const expected = new Transaction({
+      customer,
+      car,
+      dueDate,
+      amount: expectedAmount,
+    });
 
     expect(result).to.be.deep.equal(expected);
   });
